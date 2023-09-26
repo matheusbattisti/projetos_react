6f6d3656bb270345483bd48462d511451de9fdb5
Matheus Battisti <matheusbudkewicz@gmail.com>
2023-09-26 18:54:27 -0300

    ajustes

--- 2_cronometro/cronometro/src/App.jsx
+++ 2_cronometro/cronometro/src/App.jsx
@@ -1,14 +1,12 @@
-
-import './App.css'
-import Timer from './components/Timer'
+import "./App.css";
+import Timer from "./components/Timer";
 
 function App() {
-
   return (
     <>
       <Timer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
