d4be33f4e833b4d0074ddc239b1ed79f114134b7
Matheus Battisti <matheusbudkewicz@gmail.com>
2023-10-16 22:14:48 -0300

    ajustes

--- 15_tabela_ordenavel/tabelaordenavel/src/App.jsx
+++ 15_tabela_ordenavel/tabelaordenavel/src/App.jsx
@@ -10,19 +10,26 @@ const TableSortable = () => {
     { nome: "Maria", idade: 22, cargo: "Designer" },
     { nome: "Carlos", idade: 40, cargo: "Gerente" },
     { nome: "Sofia", idade: 28, cargo: "Analista" },
-    // ... outros registros
   ]);
 
   const [sortConfig, setSortConfig] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
 
+
   const sortedData = [...data].sort((a, b) => {
     if (sortConfig !== null) {
-      if (a[sortConfig.key] < b[sortConfig.key]) {
-        return sortConfig.direction === "ascending" ? -1 : 1;
-      }
-      if (a[sortConfig.key] > b[sortConfig.key]) {
-        return sortConfig.direction === "ascending" ? 1 : -1;
+      if (typeof a[sortConfig.key] === "number") {
+        // Se for número
+        return sortConfig.direction === "ascending"
+          ? a[sortConfig.key] - b[sortConfig.key]
+          : b[sortConfig.key] - a[sortConfig.key];
+      } else {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
       }
     }
     return 0;
--- 16_infinite_scroll/infinitescroll/src/components/InfiniteScroll.jsx
+++ 16_infinite_scroll/infinitescroll/src/components/InfiniteScroll.jsx
@@ -4,37 +4,52 @@ import axios from "axios";
 const InfiniteScroll = () => {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
-
   const [isLoading, setIsLoading] = useState(false);
+  const [hasMoreData, setHasMoreData] = useState(true);
 
   const fetchPosts = async () => {
+    if (isLoading) return; // Impedir chamadas simultâneas
+
     setIsLoading(true);
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
-    );
-    setPosts((prevPosts) => [...prevPosts, ...response.data]);
-    setIsLoading(false);
+    try {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
+      );
+
+      if (response.data.length > 0) {
+        setPosts((prevPosts) => [...prevPosts, ...response.data]);
+      } else {
+        setHasMoreData(false);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar os posts:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
     fetchPosts();
   }, [page]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.innerHeight + window.scrollY >=
-        document.documentElement.scrollHeight
-      ) {
-        setPage((prevPage) => prevPage + 1);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 100 &&
+      !isLoading &&
+      hasMoreData
+    ) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
 
+  useEffect(() => {
+    const throttledHandleScroll = throttle(handleScroll, 150);
+    window.addEventListener("scroll", throttledHandleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", throttledHandleScroll);
     };
-  }, []);
+  }, [hasMoreData, isLoading]);
 
   return (
     <div>
@@ -51,5 +66,16 @@ const InfiniteScroll = () => {
     </div>
   );
 };
+
+// Função para controlar a taxa de execução de outra função
+function throttle(func, delay) {
+  let lastCall = 0;
+  return function (...args) {
+    const now = new Date().getTime();
+    if (now - lastCall < delay) return;
+    lastCall = now;
+    return func(...args);
+  };
+}
 
 export default InfiniteScroll;
